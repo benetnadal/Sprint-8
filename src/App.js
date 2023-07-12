6bae5a887e099794a5a2bcbdce75f3212d90b9d4
benetnadal <benetnadal@gmail.com>
2023-07-12 23:46:37 +0200

    Exercici1 Sprint8

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import  {useEffect, useState} from 'react';
+import axios from "axios";
+import {BotoNaus, DivNaus, Header} from "./styled"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => {
+
+const [namemodel, setNamemodel] = useState([{length: null, name: null, model: null}]);//Si no és una array et donarà error per això utilitza les claus [] 
+
+ console.log(namemodel)
+
+   useEffect(() => {
+    axios.get('https://swapi.dev/api/starships/?page=1') 
+    
+    .then(res => {    
+     
+
+       setNamemodel(res.data.results.map(data => {return ({length: data.length, name: data.name , model: data.model})}))
+
+       //El length és per atribuir-li una key al llistat, no hi ha cap length igual en les 36 naus
+
+       
+
+ 
+  })}, []);  
+
+  return  (
+  
+  <DivNaus>
+
+    {/* <Header>LOGIN / SIGN UP</Header> */}
+
+{namemodel.map(info => {return(
+
+<div key={info.length}> 
+<BotoNaus>
+<p>{info.name}</p>
+<p>{info.model}</p>
+</BotoNaus>
+</div>
+)})}
+
+</DivNaus>  
+  ) 
+  };
+
 
 export default App;
+
+
+
+
---
+++ src/styled.js
@@ -0,0 +1,34 @@
+
+import styled from "styled-components"
+
+
+export const Header = styled.div`
+width: 95%;
+padding: 30px;
+background-color: #1F1F1F;
+color:white;
+text-align: end;
+`
+
+export const DivNaus = styled.div`
+display:flex; 
+flex-direction: column;
+flex-wrap: wrap;
+align-items: center;
+justify-content: center;
+width: 90%;
+margin: auto;
+background-color: #10100F
+`
+
+export const BotoNaus = styled.button`
+width:700px;
+text-align: start;
+padding:5px 20px;
+margin: 10px  auto;
+border: 2px solid transparent;
+border-radius: 5px;
+color: white;
+background-color: #1F1F1F
+`
+
